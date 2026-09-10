75ced51f221e1c1348422e452bbdf0d1a2417731
tobbarAI <tobias.barcik@gmail.com>
2026-09-10 15:11:31 +0200

    skuska

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(page_title="Dashboard")
 
@@ -20,3 +21,11 @@ except Exception as e:
 st.divider()
 
 st.write("If you can see this page, the Streamlit app itself is running correctly.")
+
+
+df = pd.DataFrame({
+    'Column 1': [1, 2, 3, 4, 5],
+    'Column 2': [6, 7, 8, 9, 10]
+})
+
+df
